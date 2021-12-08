6a63eeb761277873629eed965af84aa52d079d3a
Jim <27871987+jimf256@users.noreply.github.com>
2021-12-08 21:42:01 +0000

    python test script now uses the release executable

--- test_ai.py
+++ test_ai.py
@@ -4,7 +4,7 @@ run_count = int(input('run count: '))
 
 results = {'win': 0, 'loss': 0}
 for i in range(run_count):
-    result = subprocess.run('Binaries\Onirim_d.exe AIPlayer', shell=True)
+    result = subprocess.run('Binaries\Onirim.exe AIPlayer', shell=True)
     if result.returncode == 1:
         results['win'] += 1
     else:
