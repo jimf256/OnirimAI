4ca8b8927a69f309c273db3e2ed619b805701baa
Jim <27871987+jimf256@users.noreply.github.com>
2021-12-09 14:38:17 +0000

    added PlayerTemplate project with its own solution

---
+++ Source/PlayerTemplate/PlayerTemplate.cpp
@@ -0,0 +1,71 @@
+#include "PlayerTemplate.h"
+#include "PublicGameState.h"
+#include "LogUtils.h"
+#include <iostream>
+#include <string>
+
+// -------------------------------------------------------------------------------------------------
+
+PlayerTemplate::PlayerTemplate()
+{
+}
+
+// -------------------------------------------------------------------------------------------------
+
+void PlayerTemplate::OnGameOver(EGameResult result)
+{
+	std::cout << "Game Result: " << LogUtils::GetGameResult(result) << "\n";
+	system("pause");
+}
+
+// -------------------------------------------------------------------------------------------------
+
+void PlayerTemplate::OnCardDrawn(const Card& card)
+{
+}
+
+// -------------------------------------------------------------------------------------------------
+
+void PlayerTemplate::OnLabrynthModified(const CardCollection& labrynth)
+{
+}
+
+// -------------------------------------------------------------------------------------------------
+
+void PlayerTemplate::OnDoorModified(EColor color, EDoorModification modification)
+{
+}
+
+// -------------------------------------------------------------------------------------------------
+
+void PlayerTemplate::ResolveTurnAction(const PublicGameState& state, ETurnAction& choice, std::size_t& handIndex)
+{
+	// discard the first card in hand
+	choice = ETurnAction::DiscardCard;
+	handIndex = 0;
+}
+
+// -------------------------------------------------------------------------------------------------
+
+void PlayerTemplate::ResolveNightmareCard(const PublicGameState& state, EResolveNightmareAction& choice, EColor& color)
+{
+	// discard hand
+	choice = EResolveNightmareAction::DiscardHand;
+}
+
+// -------------------------------------------------------------------------------------------------
+
+void PlayerTemplate::ResolveDoorCard(const PublicGameState& state, const Card& doorCard, EResolveDoorAction& choice)
+{
+	// always discard a key to get a door
+	choice = EResolveDoorAction::DiscardKeyCard;
+}
+
+// -------------------------------------------------------------------------------------------------
+
+void PlayerTemplate::ResolvePremonition(const PublicGameState& state, std::vector<Card>& reorderedCards)
+{
+	// keep the current order
+}
+
+// -------------------------------------------------------------------------------------------------
---
+++ Source/PlayerTemplate/PlayerTemplate.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "PlayerInterface.h"
+
+// -------------------------------------------------------------------------------------------------
+
+class PlayerTemplate : public PlayerInterface
+{
+public:
+	PlayerTemplate();
+
+	virtual void OnGameOver(EGameResult result);
+	virtual void OnCardDrawn(const Card& card);
+	virtual void OnLabrynthModified(const CardCollection& labrynth);
+	virtual void OnDoorModified(EColor color, EDoorModification modification);
+	virtual void ResolveTurnAction(const PublicGameState& state, ETurnAction& choice, std::size_t& handIndex);
+	virtual void ResolveNightmareCard(const PublicGameState& state, EResolveNightmareAction& choice, EColor& color);
+	virtual void ResolveDoorCard(const PublicGameState& state, const Card& doorCard, EResolveDoorAction& choice);
+	virtual void ResolvePremonition(const PublicGameState& state, std::vector<Card>& reorderedCards);
+};
+
+// -------------------------------------------------------------------------------------------------
+
+extern "C" __declspec(dllexport) PlayerInterface * CreatePlayerInstance()
+{
+	return new PlayerTemplate();
+}
+
+// -------------------------------------------------------------------------------------------------
+
+extern "C" __declspec(dllexport) void DestroyPlayerInstance(PlayerInterface * player)
+{
+	delete player;
+}
+
+// -------------------------------------------------------------------------------------------------
+
