09022c5cab51c9a37a1198c40c23dc068a71fae6
Jim <27871987+jimf256@users.noreply.github.com>
2021-12-11 08:52:38 +0000

    run_ai_player.py now only launches a single process, passes a commandline argument to specify how many runs to execute, and reads results from the results.log

--- run_ai_player.py
+++ run_ai_player.py
@@ -1,27 +1,21 @@
-import multiprocessing, subprocess, time
-
-def RunSingleProcess():
-    result = subprocess.run('Binaries\Onirim.exe AIPlayer', shell=True)
-    return result.returncode
-
-def RunAllProcesses(run_count):
-    results = []
-    with multiprocessing.Pool(processes=8) as pool:
-        for i in range(run_count):
-            pool.apply_async(RunSingleProcess, callback=lambda x: results.append(x))
-        while len(results) < run_count:
-            time.sleep(0.1)
-    return results
+import subprocess, time
 
 if __name__ == '__main__':
     runs = int(input('run count: '))
     t = time.monotonic()
-    results = RunAllProcesses(runs)
-    
-    wins = results.count(1)
-    print('')
-    print(f'played: {len(results)}')
-    print(f'wins: {wins}')
-    print(f'losses: {len(results) - wins}')
-    print(f'win rate: {(wins/len(results))*100:.2f}%')
+    proc = subprocess.run(f'Binaries\Onirim.exe AIPlayer runs={runs}', shell=True)
+
+    results = {}
+    with open('results.log', 'r') as f:
+        for line in [x.strip() for x in f.readlines()]:
+            if line and len(line) > 2:
+                results[int(line[0])] = int(line[2:].strip())
+
+    wins = results[1]
+    plays = sum(results.values())
+    print('\n::: results :::')
+    print(f'played:   {plays}')
+    print(f'wins:     {wins}')
+    print(f'losses:   {plays - wins}')
+    print(f'win rate: {(wins/plays)*100:.2f}%')
     input(f'\nfinished in {time.monotonic() - t:.2f} seconds')
