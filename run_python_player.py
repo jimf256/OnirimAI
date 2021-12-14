9e08ebea64b375c60ce64c15a5a79b2c3396ad99
Jim <27871987+jimf256@users.noreply.github.com>
2021-12-14 15:15:40 +0000

    run_python_player.py now reads the results.log and prints them at the end of the run

--- run_python_player.py
+++ run_python_player.py
@@ -91,9 +91,23 @@ def RunGameInstances(runs):
     if os.path.exists(data_file) and os.path.isfile(data_file):
         os.remove(data_file)
 
+    results = {}
+    with open('results.log', 'r') as f:
+        for line in [x.strip() for x in f.readlines()]:
+            if line and len(line) > 2:
+                results[int(line[0])] = int(line[2:].strip())
+
+    wins = results[1]
+    plays = sum(results.values())
+    print('\n::: results :::')
+    print(f'played:   {plays}')
+    print(f'wins:     {wins}')
+    print(f'losses:   {plays - wins}')
+    print(f'win rate: {(wins/plays)*100:.2f}%')
+
 
 if __name__ == '__main__':
     runs = int(input('run count: '))
     t = time.monotonic()
     RunGameInstances(runs)
-    input(f'completed in {time.monotonic() - t:.2f} seconds')
+    input(f'finished in {time.monotonic() - t:.2f} seconds')
