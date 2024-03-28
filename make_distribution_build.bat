1a000e18eb07540bf7eda85c023e5a0164618589
Jim <27871987+jimf256@users.noreply.github.com>
2024-03-28 12:19:48 +0000

    fixed build process: - removed template project inclusion - fixed broken paths due to python file renaming

--- make_distribution_build.bat
+++ make_distribution_build.bat
@@ -1,6 +1,5 @@
 @echo off
 
-set /P include_template=include player template project? (y/n): 
 set /P include_python=include python player? (y/n): 
 set /P include_cpp_ai=include C++ AI player? (y/n): 
 echo.
@@ -19,33 +18,17 @@ mkdir "%build_dir%\Libraries"
 copy "Binaries\Onirim.exe" "%build_dir%\Binaries"
 copy "Libraries\ConsolePlayer.dll" "%build_dir%\Libraries"
 
-if "%include_template:~0,1%" == "y" (
-	mkdir "%build_dir%\Source\PlayerTemplate"
-	mkdir "%build_dir%\Source\GameUtils\Headers"
-	copy "Source\GameUtils\Headers" "%build_dir%\Source\GameUtils\Headers\"
-	copy "Source\PlayerTemplate" "%build_dir%\Source\PlayerTemplate\"
-	copy "Binaries\Onirim_d_nologging.exe" "%build_dir%\Binaries\Onirim_d.exe"
-	copy "Libraries\GameUtils.lib" "%build_dir%\Libraries"
-	copy "Libraries\GameUtils_d.lib" "%build_dir%\Libraries"
-
-	echo @echo off > "%build_dir%\run_debug.bat"
-	echo Binaries\Onirim_d.exe PlayerTemplate > "%build_dir%\run_debug.bat"
-
-	echo @echo off > "%build_dir%\run_release.bat"
-	echo Binaries\Onirim.exe PlayerTemplate > "%build_dir%\run_release.bat"
-)
-
 if "%include_python:~0,1%" == "y" (
 	mkdir "%build_dir%\Source\PythonPlayer"
 	copy "Source\PythonPlayer\*.py" "%build_dir%\Source\PythonPlayer\"
 	copy "Libraries\PythonPlayer.dll" "%build_dir%\Libraries"
-	copy "run_python_player.py" "%build_dir%\run_python_player.py"
+	copy "run_python_AI.py" "%build_dir%\run_python_AI.py"
 	copy "python_player_config.ini" "%build_dir%\python_player_config.ini"
 )
 
 if "%include_cpp_ai:~0,1%" == "y" (
 	copy "Libraries\AIPlayer.dll" "%build_dir%\Libraries"
-	copy "run_ai_player.py" "%build_dir%\run_ai_player.py"
+	copy "run_cpp_AI.py" "%build_dir%\run_cpp_AI.py"
 )
 
 echo @echo off > "%build_dir%\run_interactive_version.bat"
