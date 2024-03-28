1ee5d4afaf4904b60ca82b0b491d6ed9b6b76758
Jim <27871987+jimf256@users.noreply.github.com>
2024-03-28 13:06:17 +0000

    run_cpp_AI.py now chooses which player module to use by reading the cpp_player_config.ini

--- run_cpp_AI.py
+++ run_cpp_AI.py
@@ -3,13 +3,20 @@ import subprocess, time, os
 results_file = 'results.log'
 
 if __name__ == '__main__':
+
+    # determine which AI module to import via the cpp_player_config.ini file
+    ai_module_name = 'AIPlayer'
+    config_file = 'cpp_player_config.ini'
+    with open(config_file, 'r') as f:
+        ai_module_name = f.readline().strip()
+
     if os.path.exists(results_file) and os.path.isfile(results_file):
         os.remove(results_file)
 
     runs = int(input('run count: '))
     t = time.monotonic()
     devnull = subprocess.DEVNULL
-    command = f'Binaries\Onirim.exe AIPlayer runs={runs}'
+    command = f'Binaries\Onirim.exe {ai_module_name} runs={runs}'
     proc = subprocess.run(command, stdin=devnull, stdout=devnull, stderr=devnull, shell=False)
 
     results = {}
