237dff25a1ffaf08cb43b50b9b51f1f123015a10
Jim <27871987+jimf256@users.noreply.github.com>
2024-03-28 11:23:45 +0000

    fixed the clean_build_all.bat to now use VS 2022

--- clean_build_all.bat
+++ clean_build_all.bat
@@ -1,6 +1,6 @@
 @echo off
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsMSBuildCmd.bat"
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsMSBuildCmd.bat"
 
 msbuild "Source\Onirim.sln" /t:Clean /p:Configuration=Debug;Platform=x64
 msbuild "Source\Onirim.sln" /t:Clean /p:Configuration=Debug_NoLogging;Platform=x64
