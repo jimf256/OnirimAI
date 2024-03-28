80aecabce01002a7cfdb398ec665c5e441341490
Jim <27871987+jimf256@users.noreply.github.com>
2024-03-28 14:03:57 +0000

    renamed the solution to OnirimAI

--- clean_build_all.bat
+++ clean_build_all.bat
@@ -9,12 +9,12 @@ if not exist %vscmd% (
 
 call %vscmd%
 
-msbuild "Source\Onirim.sln" /t:Clean /p:Configuration=Debug;Platform=x64
-msbuild "Source\Onirim.sln" /t:Clean /p:Configuration=Debug_NoLogging;Platform=x64
-msbuild "Source\Onirim.sln" /t:Clean /p:Configuration=Release;Platform=x64
+msbuild "Source\OnirimAI.sln" /t:Clean /p:Configuration=Debug;Platform=x64
+msbuild "Source\OnirimAI.sln" /t:Clean /p:Configuration=Debug_NoLogging;Platform=x64
+msbuild "Source\OnirimAI.sln" /t:Clean /p:Configuration=Release;Platform=x64
 
-msbuild "Source\Onirim.sln" /t:Build /p:Configuration=Debug;Platform=x64
-msbuild "Source\Onirim.sln" /t:Build /p:Configuration=Debug_NoLogging;Platform=x64
-msbuild "Source\Onirim.sln" /t:Build /p:Configuration=Release;Platform=x64
+msbuild "Source\OnirimAI.sln" /t:Build /p:Configuration=Debug;Platform=x64
+msbuild "Source\OnirimAI.sln" /t:Build /p:Configuration=Debug_NoLogging;Platform=x64
+msbuild "Source\OnirimAI.sln" /t:Build /p:Configuration=Release;Platform=x64
 
 pause
